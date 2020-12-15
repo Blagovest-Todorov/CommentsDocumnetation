e523ed76c283d99b13ae58e99171f25b337761b3
Blagovest-Todorov <blagovest.stod@abv.bg>
2020-12-15 19:10:51 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Comments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ///
+            ///
+
+            // Documentation  - > is a kind of documentations what a given thing does !!!
+            // internal Documentation -> lower level ducumentation - > explains what does class., method, or piece of code
+             // External Documentation - > reqirements , design architecture, tests plan, at highr level, 
+             // strong cohesion - > single responsibility- > one class , one method to do only one thing,  not 5 .
+             // loose coupling- describes coonectivity in  our program elemetns. 
+             // if we write in good programming style our code does needs comments 
+             // the dovumentetion of the good writen code is the code itself
+             // make the code seld-documentting it, the code is easy readable and easy undersrandable
+             // do not document a bad code, rewrite it !
+             // interface is kind a like all public !
+             // th comments they dont repeat the code, good comments they explain at higher level from firt sight not seenable things.
+             // good code is self exaplainable and it does need a commets
+             // the better writen the code the less comments needed !
+
+            // Dont document the implementation details into the interface class ! interface is public !
+            // describe purpose, limitations and so on /
+
+            // Documentation in c# is made using XML 
+        }
+    }
+}
